957f1bb059a71e4ec0d245e8adba41cedd2f3dc2
aposte7 <olman4jesus@gmail.com>
2025-11-07 18:17:01 +0300

    feat: add product list with product card

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,27 +4,31 @@ import Link from 'next/link';
 import { ReactNode } from 'react';
 function layout({ children }: { children: ReactNode }) {
 	return (
-		<header className="flex px-12 py-4 bg-secondary justify-between">
-			<div className="text-3xl font-bold">
-				<Link href="/">ShopKoo</Link>
-			</div>
-			<Input
-				placeholder="Search product..."
-				className="max-w-96 rounded-full border-slate-300/80"
-				type="text"
-			/>
-			<div className="flex gap-5">
-				<Link href="chart">
-					<ShoppingCart />
-				</Link>
-				<Link href="favorites">
-					<Heart
-						className="text-red-600"
-						// fill="currentColor"
-					/>
-				</Link>
-			</div>
-		</header>
+		<>
+			<header className="flex px-12 py-4 bg-secondary justify-between">
+				<div className="text-3xl font-bold">
+					<Link href="/">ShopKoo</Link>
+				</div>
+				<Input
+					placeholder="Search product..."
+					className="max-w-96 rounded-full border-slate-300/80"
+					type="text"
+				/>
+				<div className="flex gap-5">
+					<Link href="chart">
+						<ShoppingCart />
+					</Link>
+					<Link href="favorites">
+						<Heart
+							className="text-red-600"
+							// fill="currentColor"
+						/>
+					</Link>
+				</div>
+			</header>
+
+			{children}
+		</>
 	);
 }
 
