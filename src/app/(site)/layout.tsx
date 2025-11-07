2235f9959855c5f06879578534ece9cd750bf23f
aposte7 <olman4jesus@gmail.com>
2025-11-07 16:41:59 +0300

    implement layout component with search input, and navigation links

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,6 +1,31 @@
+import { Input } from '@/components/ui/input';
+import { Heart, ShoppingCart } from 'lucide-react';
+import Link from 'next/link';
 import { ReactNode } from 'react';
 function layout({ children }: { children: ReactNode }) {
-	return <div>{children}</div>;
+	return (
+		<header className="flex px-12 py-4 bg-secondary justify-between">
+			<div className="text-3xl font-bold">
+				<Link href="/">ShopKoo</Link>
+			</div>
+			<Input
+				placeholder="Search product..."
+				className="max-w-96 rounded-full border-slate-300/80"
+				type="text"
+			/>
+			<div className="flex gap-5">
+				<Link href="chart">
+					<ShoppingCart />
+				</Link>
+				<Link href="favorites">
+					<Heart
+						className="text-red-600"
+						// fill="currentColor"
+					/>
+				</Link>
+			</div>
+		</header>
+	);
 }
 
 export default layout;
