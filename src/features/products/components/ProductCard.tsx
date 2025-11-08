3e40b9e02444f922813d76038c96c4e6e9eacf3f
aposte7 <olman4jesus@gmail.com>
2025-11-08 09:20:08 +0300

    forgot this to commit with the previous one

---
+++ src/features/products/components/ProductCard.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardFooter, CardTitle } from '@/components/ui/card';
+import { Heart, ShoppingCart, Star } from 'lucide-react';
+import Image from 'next/image';
+
+function ProductCard() {
+	return (
+		<Card className="pt-0 pb-5  gap-3">
+			<Image
+				alt="product placeholder"
+				src="https://via.placeholder.com/400x500?text=Product+Image"
+				width={400}
+				height={500}
+				className="w-full h-60  border-2 border-accent object-cover"
+				unoptimized
+			/>
+			<div className="space-y-2 px-4">
+				<div className="text-slate-400 text-sm">Essence</div>
+				<CardTitle>Essence Mascara Lash Princess</CardTitle>
+
+				<div>
+					<div className="inline-flex items-center gap-2">
+						<Star
+							className="text-yellow-400 "
+							size={16}
+							fill="currentColor"
+						/>
+						<span className="text-sm font-medium">4.5</span>
+					</div>
+				</div>
+				<div className="text-lg font-bold">120 $</div>
+			</div>
+			<CardFooter className="gap-4 px-4 py-0">
+				<Button className="flex-1">
+					<ShoppingCart /> Add To Cart
+				</Button>
+				<Button>
+					<Heart />
+				</Button>
+			</CardFooter>
+		</Card>
+	);
+}
+
+export default ProductCard;
