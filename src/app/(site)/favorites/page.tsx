e308d2b93b19c5f4134816eb88e942f4372c7d16
aposte7 <olman4jesus@gmail.com>
2025-11-10 14:36:26 +0300

    this is where Suspense fallback used ahhh

--- src/app/(site)/favorites/page.tsx
+++ src/app/(site)/favorites/page.tsx
@@ -1,9 +1,21 @@
+import React, { Suspense } from 'react';
 import FavoritesProductList from '@/features/favorites/components/FavoritesProductList';
+import { Spinner } from '@/components/ui/spinner';
 
 export const metadata = {
 	title: 'Favorites',
 };
 
 export default function FavoritesPage() {
-	return <FavoritesProductList />;
+	return (
+		<Suspense
+			fallback={
+				<div className="w-full py-12 flex items-center justify-center">
+					<Spinner className="size-7 text-primary" />
+				</div>
+			}
+		>
+			<FavoritesProductList />
+		</Suspense>
+	);
 }
